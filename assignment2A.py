03062f8f78ab8c14c880fd2ef820ec02e8e843fd
alleinzell <samuelgerrit.nellessen@gmail.com>
2023-11-28 13:23:30 +0100

    Implemented magic, square, transpose, find_max, element_mult, random_array

--- assignment2A.py
+++ assignment2A.py
@@ -39,31 +39,91 @@ def random_array(a: int, b: int, n: int):
 
 
 def element_mult(x: np.ndarray, y: np.ndarray):
-
-
+    if len(x) != len(y):
+        return None
+    else:
+        z = []
+        for i in range(len(x)):
+            z.append(x[i] * y[i]) 
+            
     return None
 
 
 def find_max(x: np.ndarray):
+    for n in x:
+        if n < 0:
+            return None
+    max_number = x[0]
+    max_index = 0
+    for i in range(len(x)):
+        if x[i] >= max_number:
+            max_index = i
+            max_number = x[i]       
     
-    
-    return None
+    return max_index
 
 
     
 def transpose(x: np.ndarray):
-    
-    return None
+    y = np.zeros((x.shape[1], x.shape[0]), dtype = int)
 
+    for row in range(x.shape[0]):
+        for col in range(x.shape[1]):
+            y[col, row] = x[row, col]    
+    return y
 
 def is_square(x: np.ndarray):
-    
-    return None
+    if len(x.shape) == 1:
+        if len(x) == 1:
+            return True
+        else:
+            return False
+        
+    for i in range(len(x.shape)):
+        if x.shape[i] != x.shape[0]:
+            return False
+
+    return True
 
     
 def is_magic(x: np.ndarray):
+    
+    if len(x.shape) != 2 or not is_square(x):
+        return None
+    
+    row_sum = x[0, :].sum()
 
-    return None
+    # row check
+    
+    for i in range(x.shape[0]):
+        if row_sum != x[i, :].sum():
+            return False
+    
+    # column check
+    
+    for i in range(x.shape[1]):
+        if row_sum != x[:, i].sum():
+            return False
+        
+    # descending diag check
+    diag_sum = 0
+    
+    for col in range(x.shape[1]):
+        diag_sum += x[col, col]
+    
+    if diag_sum != row_sum:
+        return False
+    
+    diag_sum = 0
+    
+    for i in range(x.shape[1]):
+        diag_sum += x[0+i, x.shape[0]-1-i]
+        
+    if diag_sum != row_sum:
+        return False
+    
+    
+    return True
     
     
 def main():
